bf61f096e356ee1b122c1466cb5202a652920833
Nelson Moreno <thegreyabove@gmail.com>
2022-07-08 10:39:29 -0400

    second commit

--- ChickenGoatRanchMain.java
+++ ChickenGoatRanchMain.java
@@ -7,9 +7,9 @@ public class ChickenGoatRanchMain {
         int chickenCount, goatCount;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the # of heads and legs you found in our farm today");
-        System.out.println("Enter head count: ");
+        System.out.print("Enter head count: ");
         heads = input.nextInt();
-        System.out.println("Enter leg count: ");
+        System.out.print("Enter leg count: ");
         legs = input.nextInt();
         if(legs%2 != 0)
             System.out.println("Error: Anatomically Impossible");
@@ -22,7 +22,7 @@ public class ChickenGoatRanchMain {
         if(legs != 2*chickenCount + 4*goatCount)
             System.out.println("Error: Anatomically Impossible");
         assert legs > heads : " Anatomically Impossible";
-        System.out.printf("That's a total of %d chickens and %d goats\n:", chickenCount, goatCount);
+        System.out.printf("That's a total of %d chickens and %d goats\n", chickenCount, goatCount);
     }
 
     public static int chickens(int heads, int legs) {
